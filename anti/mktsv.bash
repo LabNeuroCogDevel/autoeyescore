568d375a0ad3d4426c7446535bba7b0b774e2c33
Will Foran <willforan@gmail.com>
2013-03-20 16:41:29 -0400

    add wrapers for making tsv files from eyds

---
+++ anti/mktsv.bash
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+#
+# find all eyd files
+# convert to tsv if needed
+#
+tsvListFile="tsvs.ls"
+echo -n > $tsvListFile
+for f in $(
+         find /mnt/B/bea_res/Data/Tasks/Anti//Basic/ -maxdepth 5 -mindepth 5 -iname \*.eyd -newermt 2013-03-01| 
+          sort -t/ -k10nr # do newest first
+         ); do
+
+ echo $f
+ outdir="$(dirname "$f")/txt"
+ [ ! -d $outdir ] && mkdir -p $outdir
+
+ tsv="$outdir/$(echo $f | perl -lne 'print "$+{id}.$+{date}.anti.1.tsv" if m:/(?<id>\d{5})/(?<date>\d{8})/Raw:')"
+ echo "$tsv" >> $tsvListFile
+
+ [ -r $tsv ] && echo "skipping $tsv" && continue
+
+ set -x
+ ../../eyds/dataFromAnyEyd.pl $f > $tsv
+ set +x
+done
---
+++ bars/mktsv.bash
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+#
+# find all eyd files
+# convert to tsv if needed
+#
+for f in $(
+         find /mnt/B/bea_res/Data/Tasks/BarsBehavioral/Basic/ -maxdepth 5 -mindepth 5 -iname \*.eyd | 
+          sort -t/ -k10nr # do newest first
+         ); do
+
+ outdir="$(dirname "$f")/txt"
+ [ ! -d $outdir ] && mkdir -p $outdir
+
+ tsv="$outdir/$(echo $f | perl -lne 'print "$+{id}.$+{date}.$+{run}.tsv" if m:/(?<date>\d{8})/.*/(?<id>\d{5})[^/]*bars(?<run>\d):')"
+
+ [ -r $tsv ] && echo "skipping $tsv" && continue
+
+ echo $f \> $tsv
+ ../../eyds/dataFromAnyEyd.pl $f > $tsv
+ #break
+done
