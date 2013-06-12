6ffee85bf40df3e25a8dda516dbaa7d412d09987
Will Foran <willforan@gmail.com>
2013-06-12 12:11:51 -0400

    abstract viewdiffs, add anti (incomplete) results

---
+++ viewdiffs/checkBarsScan.R
@@ -0,0 +1,14 @@
+library(plyr)
+
+willname <- 'scannerbars'
+bname    <- 'BarsScan'
+
+# path to settings file is relative, so lets try to be in the right place
+if(any(grepl('viewdiff',getwd()))) { setwd('..'); cat('trying to move you to the correct directory\n') }
+if(!any(grepl('score/?$',getwd()))) { stop('WRONG DIR: run me from the correct directory!') }
+
+settingsfile<- sprintf('%s/%s.settings.R',willname,willname)
+taskdir     <- sprintf('bea_res/Data/Tasks/%s/Basic/',bname)
+pertrialCSV <- sprintf('%s/results/checkAgainstManual_trial.csv',willname)
+
+source('viewdiffs/showdiffs.R')
---
+++ viewdiffs/showdiffs.R
@@ -0,0 +1,62 @@
+# expect defined: 
+#settingsfile  # where are the xdat and pos settings (e.g. scannerbars.settings.R)
+#taskdir       # where on B can we find subj/date/subj.date.run.data.tsv
+#pertrialCSV   # where is the checkAgainstManual file: subj.date.run.trial acount mcount scorer
+
+# should be in same dir as ScoreRun.R
+# NB. NOT THE DIR THIS FILE IS IN
+
+source(settingsfile)
+source('ScoreRun.R')
+
+# arnold (users/lncd/rnc/ vs reese (mnt/b)
+Bdir <- '/mnt/B/'
+if(!file.exists(Bdir)) { Bdir <- '/Users/lncd/rcn/' }
+if(!file.exists(Bdir)) { stop('cannot find good location for B') }
+
+filebasedir <- sprintf('%s/%s',Bdir,taskdir)
+if(!file.exists(filebasedir)) { stop(sprintf('cannot find task directory: %s',filebasedir)) }
+
+getSacDot <- function(dotnotation) {
+ parts <- unlist(strsplit(dotnotation, '\\.'))
+ parts <- as.numeric(parts);
+ names(parts) <- c('subj','date','run','trial')
+ # filebasedir come from *settings.R file
+ dirbase  <- sprintf("%s/%s/%s",filebasedir,parts['subj'],parts['date'])
+ eyetrack <- sprintf("%s/Raw/EyeData/txt/%s.%s.%s.data.tsv",dirbase,parts['subj'],parts['date'],parts['run'])
+ saveto   <- sprintf("%s/Scored/txt/%s.%s.%s.sac.tsv",dirbase,parts['subj'],parts['date'],parts['run'])
+
+ getSacs(eyetrack,parts['subj'],parts['run'],"BarsScan",onlyontrials=parts['trial'],savedas=saveto,writetopdf=F,showplot=T,rundate=parts['date'])
+}
+
+
+n<-read.table(pertrialCSV,sep="\t",header=T)
+#sampleDifferences <-function(auto,manual) {
+#  sapply(sample(as.character(n[n$count_a==auto&n$count_m==manual,'trial']),10), function(x){a<-getSacDot(x);readline();dev.off();scoreSac(a)})
+#}
+#
+
+showdiffs <-function(auto,manual,size=10) {
+  d <- n[sample(which(n$count_a==auto&n$count_m==manual),size),]
+  ddply(d,.(trial), function(x){
+     print(x);
+     print(x$trail)
+     a<-getSacDot(as.character(x$trial));
+     readline();
+     dev.off();
+     scoreSac(a);
+   })
+}
+
+oldwd<-getwd()
+setwd(dirname(settingsfile))
+source('../compareToManual.R')
+setwd(oldwd)
+
+cat('
+
+showdiffs(-1,1,10) 
+    to see 10 instances  where auto says drop (-1), manual says correct (1)
+
+')
+
