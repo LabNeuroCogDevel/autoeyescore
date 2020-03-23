1b7a32359a8f23ac54c05cada75c4d7a518beddd
WillForan <willforan@gmail.com>
2020-03-23 16:36:55 -0400

    🚧 getSacDot... is a more flexable getSacDot -- but currently unused

--- getSacsbyDot.R
+++ getSacsbyDot.R
@@ -9,3 +9,19 @@ getSacDot <- function(dotnotation) {
 
  getSacs(eyetrack,parts['subj'],parts['run'],"BarsScan",onlyontrials=parts['trial'],savedas=saveto,writetopdf=F,showplot=T)
 }
+
+# 20200323 - more flexible getSacDot for manually inspecting AntiPet
+getSacDot... <- function(dotnotation, ...) {
+ parts <- unlist(strsplit(dotnotation, '\\.'))
+ parts <- as.numeric(parts);
+ names(parts) <- c("subj", "date", "run")
+ # filebasedir come from *settings.R file
+ fname <- paste(sep=".", parts['subj'], parts['date'], parts['run'], "data.tsv")
+ eyetrack <- file.path(filebasedir, parts["subj"], parts["date"], "Raw/EyeData/txt", fname)
+ tname <- basename(dirname(filebasedir))
+ # what to send to getSacs
+ s_args <- list(eyetrack, parts["subj"], parts["run"], tname, ...)
+ # if we have a 4th dot, it's a trial
+ if (length(parts) ==4) s_args <- c(s_args, list(onlyontrials=parts[4]))
+ do.call(getSacs, s_args)
+}
