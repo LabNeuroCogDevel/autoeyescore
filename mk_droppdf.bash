33d627e4d40b2fc0ec5f9cd0fdd30d727fbb7937
WillForan <willforan@gmail.com>
2020-04-15 20:46:06 -0400

    ✨ mk_droppdf script to collect all drop pdfs

---
+++ mk_droppdf.bash
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+[ -z "$REDO" ] && REDO=""
+set -euo pipefail
+trap 'e=$?; [ $e -ne 0 ] && echo "$0 exited in error"' EXIT
+cd $(dirname $0)
+
+#
+# concat all drops
+#  20200415WF  init  (from commands run 20200330)
+[ $# -ne 1 ] && echo "give me a task from to run on" && exit 1
+TASK="$1"
+[ ! -d $TASK ] && echo "$TASK does not exist in $(pwd)" && exit 1
+
+finalpdf=$(pwd)/${TASK}_drop.pdf
+[ -z "$REDO" -r $finalpdf ] && echo "have $finalpdf; use REDO=1 $0 $@" && exit 0
+
+eyeQC.R $TASK --pdf
+
+for s in $(pwd)/$TASK/pdf/*/; do
+   [ -r $s/drop.pdf -a -z "$REDO" ] && continue
+   pdftk concat $s/*-1.pdf cat output $s/drop.pdf
+done
+
+pdftk  /Volumes/Hera/Projects/autoeyescore/audit/$TASK/pdf/*/drop.pdf cat output $finalpdf
