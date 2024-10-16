fd86aa3cd455de0331c0b2b878eb0241c9a9a29c
WillForan <willforan@gmail.com>
2024-10-15 21:42:50 -0400

    ✨ EyeLink/inspect: work with monocular, var screen res, disp events

--- EyeLink/inspect.R
+++ EyeLink/inspect.R
@@ -7,20 +7,32 @@ restrict_range <- function(d, t_start, t_end){
       mutate(across(c(etime, stime), ~ .x - t_start))
 }
 
+paste00 <- function(...) paste0(collapse=",",...)
 
 # extract_events from eyelink_functions.R
 inspect_trial <- function(dat, only_trial=1, trial_desc="",
                           extract_events=extract_vgs_events,
-                          events=NULL) {
+                          events=NULL, score_fnc=NULL, max_x=NULL) {
     #asc_fname='example/220682rr01.asc.gz'
     #dat<- eyelinker::read.asc(asc_fname)
-    if("character" %in% class(dat)) dat <- eyelinker::read.asc(dat)
-    t_events <- extract_events(dat) %>% filter(trial == only_trial)
+    fname <- ""
+    if("character" %in% class(dat)){
+       fname <- basename(dat)
+       dat <- eyelinker::read.asc(dat)
+    }
+    t_events <- extract_events(dat) %>% filter(trial %in% only_trial)
     t_start <- min(t_events$time)
     t_end <- max(t_events$event_endtime)
 
     t_raw <- filter(dat$raw, time>=t_start, time<= t_end) %>%
         mutate(time=time-t_start)
+
+    this_score<-NULL
+    if(!is.null(score_fnc)) {
+       this_score <- score_fnc(dat) %>% filter(trial %in% only_trial)
+    }
+
+    if("info" %in% names(dat)) max_x <- dat$info$screen.x # likely 1024 or  1920 
 
     t_b <- restrict_range(dat$blinks, t_start, t_end)
     t_s <- restrict_range(dat$sacc, t_start, t_end)
@@ -32,8 +44,9 @@ inspect_trial <- function(dat, only_trial=1, trial_desc="",
                #event=factor(event, levels=seq_along(events), labels=events))
                event=as.factor(event))
 
+    t_event_plot$dotpos_adj <- (t_event_plot$dotpos+1)/2 * max_x
     #if(!'eye' %in% names(t_raw)) t_raw$eye <- 'mono'
-    ggplot(t_raw) +
+    p <- ggplot(t_raw) +
         aes(xmin=stime, xmax=etime)+
         geom_rect(data=t_b, aes(fill=eye, ymin=-100, ymax=-200),
                   alpha=.5, color="red")+
@@ -41,13 +54,24 @@ inspect_trial <- function(dat, only_trial=1, trial_desc="",
                   alpha=.5, color="blue")+
         geom_vline(data=t_event_plot,
                    aes(xintercept=time, color=event))+
-        geom_hline(data=t_event_plot[1,],
-                   aes(yintercept=dotpos),color="yellow", size=3)+
-        geom_hline(yintercept=1920/2,color="green",linetype=2)+
+        geom_line(data=t_event_plot,
+                  aes(x=time, xmin=NULL, xmax=NULL, y=dotpos_adj),color="yellow", size=3)+
+        geom_hline(yintercept=max_x/2,color="green",linetype=2)+
+        coord_cartesian(ylim=c(-200,max_x),xlim=c(0,t_end-t_start))+
+        ggtitle(paste0(collapse=" ","trial", paste00(only_trial), trial_desc)) + theme_bw()
+
+     if("xpr" %in% names(t_raw)){
+      p <- p +
         geom_point(aes(x=time, y=xpr, xmin=NULL, xmax=NULL), color="blue",alpha=.5)+
-        geom_point(aes(x=time, y=xpl, xmin=NULL, xmax=NULL), color="red",alpha=.5)+
-        coord_cartesian(ylim=c(-200,1920),xlim=c(0,t_end-t_start))+
-        ggtitle(sprintf("trial %d %s", only_trial, trial_desc)) + theme_bw()
+        geom_point(aes(x=time, y=xpl, xmin=NULL, xmax=NULL), color="red",alpha=.5)
+     } else {
+      p <- p +
+        geom_point(aes(x=time, y=xp, xmin=NULL, xmax=NULL), color="blue",alpha=.5)
+     }
+
+     if(!is.null(this_score)) p <- p +
+        ggtitle(paste0(collapse=" ",'trial', paste00(only_trial), paste00(trial_desc), 'score:', paste00(this_score$score) ,'lat:', paste00(this_score$lat),"; " fname))
+     return(p)
 }
 
 inspect_trial_test <- function(){
